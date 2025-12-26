ede274f9845e1262a196b38c0afe7024a71cf66b
Kahliya <alex.c0304@hotmail.com>
2025-12-26 16:46:59 +0800

    feat: add template pages for About & Blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router"
 import { Main } from "./pages/Main"
+import { About } from "./pages/About"
+import { Blog } from "./pages/Blog"
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/blog" element={<Blog />} />
     </Routes>
   )
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "../components/Navbar"
+
+export const About = () => {
+  return (
+    <div className="rounded-md">
+      <div className="flex flex-col">
+        <Navbar />
+        <div className="flex justify-center py-25 bg-teal-50">
+          content
+        </div>
+      </div>
+    </div>
+  )
+}
